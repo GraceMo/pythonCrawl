43030ef98d4d0e38cba5d2deb1a0c269eba31594
Grace <lvjincan556@163.com>
2019-03-03 21:07:08 +0800

    Update 人人网登陆cookiejarUrllib.py

--- 人人网登陆cookiejarUrllib.py
+++ 人人网登陆cookiejarUrllib.py
@@ -15,14 +15,14 @@ class RenRenSpider():
         self.headers = headers
 
     def get_req(self, url, data=None):
-        req = request.Request(url, headers=self.headers, data=data)
+        req = request.Request(url,data=data,headers=self.headers)
         return req
 
     def get_cookies(self):
         cookiejar = CookieJar()  # 1 创建一个cookiejar对象
         handler = request.HTTPCookieProcessor(cookiejar)  # 2 使用cookiejar创建一个HTTPCookieProcess对象
         opener = request.build_opener(handler)  # 3 使用上一步创建的handler创建一个opener
-        req = self.get_req(self.login_url, self.data)  # 4 构建Request对象
+        req = self.get_req(self.login_url, data=self.data)  # 4 构建Request对象
         opener.open(req)  # 5 请求登陆,成功之后自动携带cookies
         return opener
 
@@ -39,8 +39,8 @@ class RenRenSpider():
 if __name__ == '__main__':
     login_url = 'http://www.renren.com/PLogin.do'
     person_url = 'http://www.renren.com/969896126/profile'
-    data = {'email': 'email',
-            'password': 'password'}
+    data = {'email': '123',
+            'password': '123'}
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
     spider = RenRenSpider(login_url, person_url, headers, data)
