c8e245cce10c43372430d370701c626a8ef3025c
Grace <lvjincan556@163.com>
2019-03-29 20:19:26 +0800

    多spider运行+添加命令

--- scrapy框架/jd_book分布式/jingdong/jingdong/settings.py
+++ scrapy框架/jd_book分布式/jingdong/jingdong/settings.py
@@ -14,13 +14,15 @@ BOT_NAME = 'jingdong'
 SPIDER_MODULES = ['jingdong.spiders']
 NEWSPIDER_MODULE = 'jingdong.spiders'
 
+COMMANDS_MODULE = 'jingdong.mycmd'
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
-LOG_LEVEL = 'WARNING'
-LOG_FILE = 'JDdd.log'
+# LOG_LEVEL = 'WARNING'
+# LOG_FILE = 'JDdd.log'
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
 
